132c35697ff1cbfea17740474d387c7ca3dfae09
ofir <yaishof@post.bgu.ac.il>
2019-07-28 12:08:39 +0300

    New files added

--- src/graphic.erl
+++ src/graphic.erl
@@ -1,143 +1,223 @@
+%%%-------------------------------------------------------------------
+%%% @author ofir
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Jul 2019 9:42 PM
+%%%-------------------------------------------------------------------
 -module(graphic).
--behevior(gen_fsm).
--compile(export_all).
+-author("ofir").
+
+-behaviour(gen_statem).
+
+%% API
+-export([start_link/0]).
+
+%% gen_statem callbacks
+-export([
+  init/1,
+  packetsDeliver/3,
+  terminate/3,
+  code_change/4,
+  callback_mode/0
+]).
 -include_lib("wx/include/wx.hrl").
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%test
-test() ->
-	Pid= spawn_link(fun()->init([]) end),
-	CitiesData = [alive, alive, alive, alive, alive, destroyed, destroyed, destroyed, alive, alive],
-	loopTest(Pid, 0, CitiesData).
-
-loopTest(Pid, Iteration, CitiesData) ->
-	Pid ! frameDateStart,
-	Pid ! {citiesData, CitiesData},
-	%%Pid ! {missaleData, {interceptor, Iteration, Iteration, 30}},
-	%%Pid ! {missaleData, {enemyMissile,Iteration+70, Iteration+70, 70}},
-	%%Pid ! {explosionsData, {explode, Iteration+130, Iteration+130}},
-	Pid ! frameDataEnd,
-	timer:sleep(10),
-	loopTest(Pid, Iteration+1, CitiesData).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-start_link()->
-	gen_fsm:start_link(?MODULE,[],[]).
-
-%create the GUI
-init([])->
-	wx:new(),
-	WxEnv = wx:get_env(),
-	Frame = wxFrame:new(wx:null(), -1, "world :D", [{size,{1200, 850}}]),
-	Panel  = wxPanel:new(Frame),
-	Canvas = wxPanel:new(Panel, [{size, {1200, 800}}]),
-	CitiesImages = loadCitiesImages(),
-	MissileAndExplosionImages = loadMissileAndExplosionImages(),
-	%%spawn_link(fun()->draw_buttuns(Panel,WxEnv,PlayersPNGs) end), %
-	wxFrame:show(Frame),
-	wxPanel:connect(Canvas, paint, []),
-	Background =wxImage:new("include/worldBackground.png"), %background
-	ClientDC = wxClientDC:new(Canvas),
-	BackgroundBitmap = wxBitmap:new(Background), %convert to bitmap
-	wxDC:drawBitmap(ClientDC, BackgroundBitmap, {0,0}),
-	wxClientDC:destroy(ClientDC),
-	%%{ok,appear,[Background,Canvas,0],0}.
-	frameLoop(WxEnv,BackgroundBitmap, Canvas, CitiesImages, MissileAndExplosionImages). %%TODO maybe use data structure instead of delivering the images through function call
+start_link() ->
+  gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
-
-%in this function we update the graphics
-frameLoop (WxEnv, BackgroundBitmap, Canvas, CitiesImages, MissileAndExplosionImages) ->
-	ClientDC = wxClientDC:new(Canvas),
-	BufferDC = wxBufferedDC:new(ClientDC),
-	wxDC:drawBitmap(BufferDC, BackgroundBitmap, {0,0}),
-  receive
-		frameDateStart ->
-			updateDataLoop(WxEnv,BufferDC, CitiesImages, MissileAndExplosionImages)
-	end,
-	wxBufferedDC:destroy(BufferDC),
-	wxClientDC:destroy(ClientDC),
-	frameLoop(WxEnv, BackgroundBitmap, Canvas, CitiesImages, MissileAndExplosionImages).
-
-
-%gets all the  data needed for the next frame
-updateDataLoop(WxEnv, BufferDC, CitiesImages, MissileAndExplosionImages) ->
-	receive
-		{citiesData, CitiesData} ->
-			%spawn_link(fun()->drawCities(WxEnv, BufferDC, CitiesImages, CitiesData, 10) end), %%TODO: check if drawing to bufferDC can be done in parallel
-			drawCities (WxEnv, BufferDC, CitiesImages, CitiesData, 10),
-			updateDataLoop (WxEnv, BufferDC, CitiesImages, MissileAndExplosionImages);
-		{missaleData, MissileLocationData} ->
-			%spawn_link(fun()->drawMissiles (WxEnv, BufferDC, MissileAndExplosionImages, MissileLocationData) end),
-			drawMissiles (WxEnv, BufferDC, MissileAndExplosionImages, MissileLocationData),
-			updateDataLoop (WxEnv, BufferDC, CitiesImages, MissileAndExplosionImages);
-		{explosionsData,ExplosionLocationData} ->
-			%spawn_link(fun()->drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, ExplosionLocationData) end),
-			drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, ExplosionLocationData),
-			updateDataLoop (WxEnv, BufferDC, CitiesImages, MissileAndExplosionImages);
-		frameDataEnd -> frameDataEnd %%TODO: maybe we should ensure that all the processes finished their work their work if we do in in parallel.
-	end.
+init([]) ->
+  wx:new(),
+  WxEnv = wx:get_env(),
+  Frame = wxFrame:new(wx:null(), -1, "world :D", [{size,{1200, 850}}]),
+  Panel  = wxPanel:new(Frame),
+  Canvas = wxPanel:new(Panel, [{size, {1200, 800}}]),
+  CitiesImages = loadCitiesImages(),
+  MissileAndExplosionImages = loadMissileAndExplosionImages(),
+  RadarsImages=loadRadarsImages(),
+  LaunchersImages=loadLaunchersImages(),
+  %%spawn_link(fun()->draw_buttuns(Panel,WxEnv,PlayersPNGs) end), %
+  wxFrame:show(Frame),
+  wxPanel:connect(Canvas, paint, []),
+  Background =wxImage:new("include/worldBackground.png"), %background
+  ClientDC = wxClientDC:new(Canvas),
+  BackgroundBitmap = wxBitmap:new(Background), %convert to bitmap
+  wxDC:drawBitmap(ClientDC, BackgroundBitmap, {0,0}),
+  wxClientDC:destroy(ClientDC),
+  Data= #{citiesImages =>CitiesImages, missileAndExplosionImages=> MissileAndExplosionImages,
+    radarsImages => RadarsImages, launchersImages => LaunchersImages,
+    wxEnv => WxEnv, backgroundBitmap =>BackgroundBitmap, canvas => Canvas, numOfPacketsDelivered => 0},
+  {ok, packetsDeliver, Data}.
 
 
-%draw Cities function
-drawCities (_WxEnv,_BufferDC, _CitiesImages, _CitiesData, 0) ->
-	finishedDrawingCities;
+callback_mode() ->
+  state_functions.
 
-drawCities (WxEnv,BufferDC, CitiesImages, [CityDate|OtherCitiesDate], NumberOfCity) ->
-	wx:set_env(WxEnv),
-	case CityDate of
-		destroyed ->
-			drawCities (WxEnv, BufferDC, CitiesImages, OtherCitiesDate, NumberOfCity-1);
-		alive ->
-			wxDC:drawBitmap(BufferDC, element(NumberOfCity,CitiesImages), {0,0}),
-			drawCities (WxEnv, BufferDC, CitiesImages, OtherCitiesDate, NumberOfCity-1)
-	end.
+packetsDeliver(info, _OldState, Data) ->
+  {keep_state, Data};
 
-%draw Missiles function
-drawMissiles (WxEnv, BufferDC, MissileAndExplosionImages, {MissileType, X, Y, Angle}) ->
-	wx:set_env(WxEnv),
-	case MissileType of
-		interceptor ->
-			Image= wxImage:rotate (element(1,MissileAndExplosionImages), Angle, {31, 48}); %%rotate(This, Angle, Centre_of_rotation) %%TODO: check the angle unit, make sure you choose the right center
-		enemyMissile ->
-			Image= wxImage:rotate (element(2,MissileAndExplosionImages), Angle, {31, 48})
-	end,
-	Bitmap= wxBitmap:new(Image),
-	wxDC:drawBitmap(BufferDC, Bitmap, {X,Y}), %%TODO: check maybe it is the opposite
-	wxBitmap:destroy(Bitmap).
+packetsDeliver(cast, PacketData, Data) ->
+  io:format ("Key0", []),
+  NumOfPacketsDelivered=maps:get(numOfPacketsDelivered, Data),
+  NewData=
+    if
+      NumOfPacketsDelivered =:= 0 -> (preparationForPacketDeliver (Data));
+      true -> Data#{ numOfPacketsDelivered := NumOfPacketsDelivered +1}
+    end,
+  draw(PacketData,NewData),
+  NewNumOfPacketsDelivered=maps:get(numOfPacketsDelivered, NewData),
+    if
+      NewNumOfPacketsDelivered =:= 4 ->
+        finishDrawing(NewData),
+        {keep_state, NewData#{numOfPacketsDelivered := 0}};
+      true ->
+        {keep_state, NewData}
+    end.
 
-%draw Explosions function
-drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, {ExplosionType, X, Y}) ->
-	wx:set_env(WxEnv),
-	case ExplosionType of
-		interception ->
-			wxDC:drawBitmap(BufferDC, element(3,MissileAndExplosionImages), {X, Y});
-		explode ->
-			wxDC:drawBitmap(BufferDC, element(4,MissileAndExplosionImages), {X,Y})
-	end.
 
-%%returns bitmaps of the cities images
+
+terminate(_Reason, _StateName, _State) ->
+  ok.
+
+code_change(_OldVsn, StateName, State, _Extra) ->
+  {ok, StateName, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+%%returns map of bitmaps of the cities images
 loadCitiesImages()->
-	{
-		wxBitmap:new(wxImage:new("include/budapestE.png")),
-		wxBitmap:new(wxImage:new("include/jerusalemE.png")),
-		wxBitmap:new(wxImage:new("include/londonE.png")),
-		wxBitmap:new(wxImage:new("include/moscowE.png")),
-		wxBitmap:new(wxImage:new("include/newYorkE .png")),
-		wxBitmap:new(wxImage:new("include/ParisE.png")),
-		wxBitmap:new(wxImage:new("include/romeE.png")),
-		wxBitmap:new(wxImage:new("include/stockholmE.png")),
-		wxBitmap:new(wxImage:new("include/sydneyE.png")),
-		wxBitmap:new(wxImage:new("include/WashingtonE.png"))
-	}.
+  #{
+    budapest    => wxBitmap:new(wxImage:new("include/budapestE.png")),
+    jerusalem   => wxBitmap:new(wxImage:new("include/jerusalemE.png")),
+    london      => wxBitmap:new(wxImage:new("include/londonE.png")),
+    moscow      => wxBitmap:new(wxImage:new("include/moscowE.png")),
+    newYork     => wxBitmap:new(wxImage:new("include/newYorkE .png")),
+    paris       => wxBitmap:new(wxImage:new("include/ParisE.png")),
+    rome        => wxBitmap:new(wxImage:new("include/romeE.png")),
+    stockholm   => wxBitmap:new(wxImage:new("include/stockholmE.png")),
+    sydney      => wxBitmap:new(wxImage:new("include/sydneyE.png")),
+    washington  => wxBitmap:new(wxImage:new("include/WashingtonE.png"))
+  }.
 
+%%returns map of bitmaps of the radars images
+loadRadarsImages()->
+  #{
+    radar1    => wxBitmap:new(wxImage:new("include/radar1.png")),
+    radar2    => wxBitmap:new(wxImage:new("include/radar2.png")),
+    radar3    => wxBitmap:new(wxImage:new("include/radar3.png")),
+    radar4    => wxBitmap:new(wxImage:new("include/radar4.png"))
+  }.
+
+%%returns map of bitmaps of the launchers images
+loadLaunchersImages()->
+  #{
+    launcher1 => wxBitmap:new(wxImage:new("include/launcher1.png")),
+    launcher2 => wxBitmap:new(wxImage:new("include/launcher2.png")),
+    launcher3 => wxBitmap:new(wxImage:new("include/launcher3.png")),
+    launcher4 => wxBitmap:new(wxImage:new("include/launcher4.png"))
+  }.
 
 %%returns bitmaps of the Missile And Explosion images
 loadMissileAndExplosionImages()->
-	{
-		wxImage:new("include/interceptor.png"),
-		wxImage:new("include/enemyMissile.png"),
-		wxBitmap:new(wxImage:new("include/interception.png")),
-		wxBitmap:new(wxImage:new("include/explode.png"))
-	}.
+  {
+    wxImage:new("include/interceptor.png"),
+    wxImage:new("include/enemyMissile.png"),
+    wxBitmap:new(wxImage:new("include/interception.png")),
+    wxBitmap:new(wxImage:new("include/explode.png"))
+  }.
+
+preparationForPacketDeliver (Data) ->
+  BackgroundBitmap=maps:get(backgroundBitmap, Data),
+  Canvas=maps:get(canvas, Data),
+  ClientDC = wxClientDC:new(Canvas),
+  BufferDC = wxBufferedDC:new(ClientDC),
+  wxDC:drawBitmap(BufferDC, BackgroundBitmap, {0,0}),
+  Data#{ clientDC => ClientDC ,bufferDC => BufferDC, numOfPacketsDelivered => 1}.
+
+
+%draw Cities function
+drawCitiesOrRadarsOrLaunchers(_WxEnv,_BufferDC, _Images, []) ->
+  finishedDrawingElements;
+
+drawCitiesOrRadarsOrLaunchers(WxEnv,BufferDC, Images, [ElementData|OtherElementsData]) ->
+  wx:set_env(WxEnv),
+  case element(2, ElementData) of
+    destroyed ->
+      drawCitiesOrRadarsOrLaunchers (WxEnv, BufferDC, Images, OtherElementsData);
+    alive ->
+      wxDC:drawBitmap(BufferDC, maps:get(element(1, ElementData),Images), {0,0}),
+      drawCitiesOrRadarsOrLaunchers (WxEnv, BufferDC, Images, OtherElementsData)
+  end.
+
+%draw AntiMissiles function
+drawAntiMissiles (_WxEnv, _BufferDC, _MissileAndExplosionImages, []) ->
+  finishedDrawingAntiMissiles;
+
+drawAntiMissiles (WxEnv, BufferDC, MissileAndExplosionImages, [{X, Y, Angle}|OtherAntiMissiles]) ->
+  wx:set_env(WxEnv),
+  Image= wxImage:rotate (element(1,MissileAndExplosionImages), Angle, {31, 48}), %%rotate(This, Angle, Centre_of_rotation) %%TODO: check the angle unit, make sure you choose the right center
+  Bitmap= wxBitmap:new(Image),
+  wxDC:drawBitmap(BufferDC, Bitmap, {X,Y}), %%TODO: check maybe it is the opposite
+  wxBitmap:destroy(Bitmap),
+  drawAntiMissiles (WxEnv, BufferDC, MissileAndExplosionImages, OtherAntiMissiles).
+
+
+%draw Missiles function
+drawMissiles (_WxEnv, _BufferDC, _MissileAndExplosionImages, []) ->
+  finishedDrawingMissiles;
+
+drawMissiles (WxEnv, BufferDC, MissileAndExplosionImages, [{X, Y, Angle}|OtherMissiles]) ->
+  wx:set_env(WxEnv),
+  Image= wxImage:rotate (element(2,MissileAndExplosionImages), Angle, {26, 42}), %%rotate(This, Angle, Centre_of_rotation) %%TODO: check the angle unit, make sure you choose the right center
+  Bitmap= wxBitmap:new(Image),
+  wxDC:drawBitmap(BufferDC, Bitmap, {X,Y}), %%TODO: check maybe it is the opposite
+  wxBitmap:destroy(Bitmap),
+  drawAntiMissiles (WxEnv, BufferDC, MissileAndExplosionImages, OtherMissiles).
+
+
+%draw Interceptions function
+drawInterceptions(_WxEnv, _BufferDC, _MissileAndExplosionImages, []) ->
+  finishedDrawingExplosions;
+
+drawInterceptions(WxEnv, BufferDC, MissileAndExplosionImages, [{X, Y}|OtherInterceptions]) ->
+  wx:set_env(WxEnv),
+  wxDC:drawBitmap(BufferDC, element(3,MissileAndExplosionImages), {X,Y}),
+  drawInterceptions(WxEnv, BufferDC, MissileAndExplosionImages, OtherInterceptions).
+
+
+%draw Explosions function
+drawExplosions(_WxEnv, _BufferDC, _MissileAndExplosionImages, []) ->
+  finishedDrawingExplosions;
+
+drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, [{X, Y}|OtherExplosions]) ->
+  wx:set_env(WxEnv),
+  wxDC:drawBitmap(BufferDC, element(4,MissileAndExplosionImages), {X,Y}),
+  drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, OtherExplosions).
+
+
+draw(PacketData, Data) ->
+  CitiesImages=maps:get(citiesImages, Data),
+  MissileAndExplosionImages=maps:get(missileAndExplosionImages, Data),
+  RadarsImages=maps:get(radarsImages, Data),
+  LaunchersImages=maps:get(launchersImages, Data),
+  WxEnv=maps:get(wxEnv, Data),
+  BufferDC=maps:get(bufferDC, Data),
+  drawCitiesOrRadarsOrLaunchers (WxEnv, BufferDC, LaunchersImages, element(1, PacketData)),
+  drawCitiesOrRadarsOrLaunchers (WxEnv, BufferDC, RadarsImages, element(2, PacketData)),
+  drawCitiesOrRadarsOrLaunchers (WxEnv, BufferDC, CitiesImages, element(3, PacketData)),
+  drawAntiMissiles (WxEnv, BufferDC, MissileAndExplosionImages, element(4, PacketData)),
+  drawMissiles (WxEnv, BufferDC, MissileAndExplosionImages, element(5, PacketData)),
+  drawInterceptions(WxEnv, BufferDC, MissileAndExplosionImages, element(6, PacketData)),
+  drawExplosions(WxEnv, BufferDC, MissileAndExplosionImages, element(7, PacketData)).
+
+finishDrawing(Data) ->
+  BufferDC=maps:get(bufferDC, Data),
+  ClientDC=maps:get(clientDC, Data),
+  wxBufferedDC:destroy(BufferDC),
+  wxClientDC:destroy(ClientDC).
